f32d54a5dc38e84c5f199cc849276904003903bb
Babitha <babithababi340@gmail.com>
2024-11-29 13:01:59 -0700

    work on Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import "./Header.css";
+import logo from "../../assets/headerLogo.svg";
+import avatar from "../../assets/headerAvatar.svg";
+
+function Header() {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="Header Logo" />
+      <p className="header__date-and-location">{currentDate}, Location</p>
+      <button className="header__add-clothes-btn"> + Add clothes</button>
+      <div className="header__user-containter">
+        <p className="header__username">Babitha Vaithy</p>
+        <img src={avatar} alt="Babitha Vaithy" className="header__avatar" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,12 @@
+import Sunny from "../../assets/tempSunny.svg";
+
+function WeatherCard() {
+  return (
+    <section className="weather__card">
+      <p className="weather-card__temp">75 &deg; F</p>
+      <img src={Sunny} alt="Temp Sunny" className="weather-card__image" />
+    </section>
+  );
+}
+
+export default WeatherCard;
