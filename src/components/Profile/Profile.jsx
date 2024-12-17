287cd810e70168f84657d2223506a69899f4bc65
Babitha <babithababi340@gmail.com>
2024-12-17 16:56:24 -0700

    Update on Profile.css import

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SideBar from "../SideBar/SideBar";
 import ClothesSection from "../ClothesSection/ClothesSection";
-import "./profile.css";
+import "./Profile.css";
 
 function Profile({ handleCardClick }) {
   return (
